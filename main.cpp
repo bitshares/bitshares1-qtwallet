71802675dccdf1f9db45e40826c08ef31e77431b
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-15 14:55:44 -0400

    update Qt wallet to work again

--- main.cpp
+++ main.cpp
@@ -37,6 +37,7 @@
 #include <boost/iostreams/stream.hpp>
 #include <fstream>
 
+#include <QResource>
 #include <iostream>
 #include <iomanip>
 
@@ -46,140 +47,82 @@
 #include "config_dev.hpp"
 #endif
 
-
-struct config
+void get_htdocs_file( const fc::path& filename, const fc::http::server::response& r )
 {
-   config( ) 
-   : default_peers{{"107.170.30.182:8764"},{"114.215.104.153:8764"},{"84.238.140.192:8764"}},
-    splash_screen_path( (fc::path(package_dir) / "images/splash_screen.png").string() )
-   {}
-
-   void init_default_logger( const fc::path& data_dir )
+//   elog( "${file}", ("file",filename) );
+   std::cout << filename.generic_string() << "\n";
+   QResource  file_to_send( ("/htdocs/htdocs/" + filename.generic_string()).c_str() );
+   if( !file_to_send.data() )
    {
-          fc::logging_config cfg;
-          
-          fc::file_appender::config ac;
-          ac.filename = data_dir / "default.log";
-          ac.truncate = false;
-          ac.flush    = true;
-
-          std::cout << "Logging to file \"" << ac.filename.generic_string() << "\"\n";
-          
-          fc::file_appender::config ac_rpc;
-          ac_rpc.filename = data_dir / "rpc.log";
-          ac_rpc.truncate = false;
-          ac_rpc.flush    = true;
-
-          std::cout << "Logging RPC to file \"" << ac_rpc.filename.generic_string() << "\"\n";
-          
-          cfg.appenders.push_back(fc::appender_config( "default", "file", fc::variant(ac)));
-          cfg.appenders.push_back(fc::appender_config( "rpc", "file", fc::variant(ac_rpc)));
-          
-          fc::logger_config dlc;
-          dlc.level = fc::log_level::debug;
-          dlc.name = "default";
-          dlc.appenders.push_back("default");
-          
-          fc::logger_config dlc_rpc;
-          dlc_rpc.level = fc::log_level::debug;
-          dlc_rpc.name = "rpc";
-          dlc_rpc.appenders.push_back("rpc");
-          
-          cfg.loggers.push_back(dlc);
-          cfg.loggers.push_back(dlc_rpc);
-          
-          logging = cfg;
+      std::string not_found = "this is not the file you are looking for: " + filename.generic_string();
+      r.set_status( fc::http::reply::NotFound );
+      r.set_length( not_found.size() );
+      r.write( not_found.c_str(), not_found.size() );
+      return;
+   }  
+   r.set_status( fc::http::reply::OK );
+   if( file_to_send.isCompressed() )
+   {
+      auto data = qUncompress( file_to_send.data(), file_to_send.size() );
+      r.set_length( data.size() );
+      r.write( (const char*)data.data(), data.size() );
    }
-   std::vector<std::string>     default_peers;
-   fc::logging_config           logging;
-   fc::string                   splash_screen_path; 
-};
-
-FC_REFLECT( config, (default_peers)(logging)(splash_screen_path) )
-
-
-static config load_config( const fc::path& data_dir )
-{
-  auto config_file = data_dir / "config.json";
-  config cfg;
-  if( fc::exists( config_file ) )
-  {
-     std::cout << "Loading config \"" << config_file.generic_string()  << "\"\n";
-     cfg = fc::json::from_file( config_file ).as<config>();
-  }
-  else
-  {
-     std::cerr<<"Creating default config file \""<<fc::absolute(config_file).generic_string()<<"\"\n";
-     cfg.init_default_logger( data_dir );
-     fc::create_directories( config_file.parent_path() );
-     fc::json::save_to_file( cfg, fc::absolute(config_file) );
-     std::cerr << "done saving config\n";	
-  }
-  fc::configure_logging( cfg.logging );
-  return cfg;
+   else
+   {
+      r.set_length( file_to_send.size() );
+      r.write( (const char*)file_to_send.data(), file_to_send.size() );
+   }
 }
+
 
 int main( int argc, char** argv )
 {
-    
-    QCoreApplication::setOrganizationName( "BitShares" );
-    QCoreApplication::setOrganizationDomain( "bitshares.org" );
-    QCoreApplication::setApplicationName( BTS_BLOCKCHAIN_NAME );
-    QApplication app(argc, argv);
+   QCoreApplication::setOrganizationName( "BitShares" );
+   QCoreApplication::setOrganizationDomain( "bitshares.org" );
+   QCoreApplication::setApplicationName( BTS_BLOCKCHAIN_NAME );
+   QApplication app(argc, argv);
+
+   auto data_dir = fc::app_path() / BTS_BLOCKCHAIN_NAME;
+
+   QPixmap pixmap(":/images/splash_screen.png");
+   QSplashScreen splash(pixmap);
+      splash.showMessage(QObject::tr("Loading configuration..."),
+                         Qt::AlignCenter | Qt::AlignBottom, Qt::white);   
+   splash.show();
+
+   QSettings settings; 
+   bool      upnp    = settings.value( "network/p2p/use_upnp", true ).toBool();
+   uint32_t  p2pport = settings.value( "network/p2p/port", BTS_NETWORK_DEFAULT_P2P_PORT ).toInt();
     
    try {
-    auto data_dir = fc::app_path() / "BitShares" BTS_ADDRESS_PREFIX;
-
-    config cfg = load_config( data_dir );
-
     std::shared_ptr<bts::client::client> client;
-    bts::rpc::rpc_server_ptr                rpc_server;
 
-    bts::rpc::rpc_server::config rpc;
-#   ifdef NDEBUG
-    rpc.rpc_user     = "randomuser";
-    rpc.rpc_password = fc::variant(fc::ecc::private_key::generate()).as_string();
-    rpc.httpd_endpoint = fc::ip::endpoint::from_string( "127.0.0.1:9999" );
-    rpc.httpd_endpoint.set_port(0);
-#   else
-    rpc.rpc_user     = "user";
-    rpc.rpc_password = "pass";
-    rpc.httpd_endpoint = fc::ip::endpoint::from_string( "0.0.0.0:5680" );
-    rpc.httpd_endpoint.set_port(5680);
-#   endif
-    rpc.htdocs = fc::path( package_dir ) / "htdocs";
-    ilog( "htdocs: ${d}", ("d",rpc.htdocs) );
+    bts::client::config cfg;
+    cfg.rpc.rpc_user     = "randomuser";
+    cfg.rpc.rpc_password = fc::variant(fc::ecc::private_key::generate()).as_string();
+    cfg.rpc.httpd_endpoint = fc::ip::endpoint::from_string( "127.0.0.1:9999" );
+    cfg.rpc.httpd_endpoint.set_port(0);
+    ilog( "config: ${d}", ("d", fc::json::to_pretty_string(cfg) ) );
        
-    bool rpc_success = false;
     fc::optional<fc::ip::endpoint> actual_httpd_endpoint;
-       
-    QSettings settings; 
-
-    //uint32_t p2pport = settings.value( "network/p2p/port", BTS_NETWORK_DEFAULT_P2P_PORT ).toInt();
-    bool     upnp    = settings.value( "network/p2p/use_upnp", true ).toBool();
-
     
-    QString splash_screen_path = cfg.splash_screen_path.c_str();
-        
-    // TODO: splash_screen.png's path should be loaded from config
-    QPixmap pixmap(splash_screen_path);
-    QSplashScreen splash(pixmap);
-       splash.showMessage(QObject::tr("Starting RPC Server..."),
-                          Qt::AlignCenter | Qt::AlignBottom, Qt::white);   
-    
-    splash.show();
-    
+    fc::thread& main_thread = fc::thread::current();
 
     fc::thread bitshares_thread( "bitshares" );
     bitshares_thread.async( [&](){
+
       client = std::make_shared<bts::client::client>();
       client->open( data_dir );
-      client->run_delegate();
+      client->sync_with_ntp();
 
-      rpc_success = client->get_rpc_server()->configure( rpc );
-
+      // setup  RPC / HTTP services
+      client->get_rpc_server()->set_http_file_callback( get_htdocs_file );
+      client->get_rpc_server()->configure( cfg.rpc );
       actual_httpd_endpoint = client->get_rpc_server()->get_httpd_endpoint();
+
+      // load config for p2p node.. creates cli
       client->configure( data_dir );
+      client->init_cli();
 
       client->listen_on_port(0);
       fc::ip::endpoint actual_p2p_endpoint = client->get_p2p_listening_endpoint();
@@ -193,16 +136,21 @@ int main( int argc, char** argv )
 
       for (std::string default_peer : cfg.default_peers)
         client->connect_to_peer(default_peer);
+
+      client->run_delegate();
     }).wait();
 
-    if( !actual_httpd_endpoint ) return -1;
-       
-    ilog( "http rpc url: ${url}", ("url",actual_httpd_endpoint) );
+    if( !actual_httpd_endpoint )
+    {
+       splash.showMessage(QObject::tr("Unable to start HTTP server..."),
+                             Qt::AlignCenter | Qt::AlignBottom, Qt::white);
+       fc::usleep( fc::seconds(3) );
+       return -1;
+    }
+
     std::cout << "http rpc url: http://" << std::string( *actual_httpd_endpoint ) << std::endl;
     
     QString initial_url = ("http://" + std::string( *actual_httpd_endpoint )).c_str();
-    if( !fc::exists( data_dir / "wallets" / "default" ) )
-        initial_url += "/blank.html#/createwallet";
         
     qApp->processEvents();
     
@@ -215,8 +163,8 @@ int main( int argc, char** argv )
     
     
     QUrl url = QUrl(initial_url);
-    url.setUserName(rpc.rpc_user.c_str() );
-    url.setPassword(rpc.rpc_password.c_str() );
+    url.setUserName(cfg.rpc.rpc_user.c_str() );
+    url.setPassword(cfg.rpc.rpc_password.c_str() );
     viewer.loadUrl(url);
     
     splash.finish(&viewer);
