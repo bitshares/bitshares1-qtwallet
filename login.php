53f3d783204187828af64aa36f46c3dfe372408b
Nathan Hourt <nathan@bitshares.org>
2014-07-23 15:06:58 -0400

    Update login.php to use EasyBitcoin RPC library

--- login.php
+++ login.php
@@ -1,55 +1,42 @@
 <//-- This is a proof of concept web application for BitShares logins. -->
 <?PHP
     //CONFIG
-    $RPC_SERVER_IP = "127.0.0.1";
-    $RPC_SERVER_PORT = 50837;
-    $RPC_SERVER_USER = "server";
-    $RPC_SERVER_PASS = "server";
+    $RPC_SERVER_USER = "a";
+    $RPC_SERVER_PASS = "a";
+    $RPC_SERVER_ADDRESS = "localhost";
+    $RPC_SERVER_PORT = 13175;
+    $RPC_SERVER_PATH = "rpc";
     $RPC_SERVER_WALLET = "server";
     $RPC_SERVER_WALLET_PASS = "iamtheserver";
 
-    $BITSHARES_USER_NAME = "server.com";
+    $BITSHARES_USER_NAME = "test-server";
 
     $LOGIN_PAGE_DOMAIN_AND_PATH = "localhost:8888/login.php";
     //END CONFIG
 
-    $sock = socket_create(AF_INET, SOCK_STREAM, getprotobyname("tcp"));
-    socket_connect($sock, $RPC_SERVER_IP, $RPC_SERVER_PORT);
-    
-    function rpc($method, $params = array())
-    {
-        global $sock;
-        $json = json_encode(array('method'=>$method,'params'=>$params,'id'=>1));
-        socket_write($sock, $json);
-        $json = json_decode(socket_read($sock, 99999, PHP_NORMAL_READ), true);
-        return $json["result"];
-    }
-    
-    rpc("login", [$RPC_SERVER_USER, $RPC_SERVER_PASS]);
-    rpc("open", [$RPC_SERVER_WALLET]);
-    rpc("unlock", ["2", $RPC_SERVER_WALLET_PASS]);
+    // Using EasyBitcoin RPC client from https://github.com/aceat64/EasyBitcoin-PHP
+    require_once "includes/easybitcoin.php";
+    $bitshares = new Bitcoin($RPC_SERVER_USER, $RPC_SERVER_PASS, $RPC_SERVER_ADDRESS, $RPC_SERVER_PORT, $RPC_SERVER_PATH);
+
+    $bitshares->open($RPC_SERVER_WALLET);
+    $bitshares->unlock(2, $RPC_SERVER_WALLET_PASS);   
     
     if(isset($_REQUEST["client_key"]))
     {
-        $loginPackage = rpc("wallet_login_finish", [$_REQUEST["server_key"], $_REQUEST["client_key"], $_REQUEST["signed_secret"]]);
-        if(!isset($loginPackage))
+        $loginPackage = $bitshares->wallet_login_finish($_REQUEST["server_key"], $_REQUEST["client_key"], $_REQUEST["signed_secret"]);
+        if($loginPackage == false)
         {
             echo"Sorry, your login session has expired. Redirecting you to login again.<script type='text/javascript'>setTimeout(function(){location.href='$_SERVER[PHP_SELF]'}, 3000)</script>";
-            socket_close($sock);
             return;
         }
-        $userAccount = rpc("blockchain_get_account", [$loginPackage["user_account_key"]]);
+        $userAccount = $bitshares->blockchain_get_account($loginPackage["user_account_key"]);
         echo "Welcome, $_REQUEST[client_name]! You are now logged in.<br/>";
-        if(!isset($userAccount))
-            echo "You have not registered your name with BitShares X.<br/>";
-        else {
-            if(isset($userAccount["public_data"]["gravatarID"]))
-                echo "<img src='http://www.gravatar.com/avatar/{$userAccount["public_data"]["gravatarID"]}' />";
-            if(isset($userAccount["delegate_info"]))
-                echo "<p>This is the VIP section, for delegates only.</p>";
-        }
+        if(isset($userAccount["public_data"]["gravatarID"]))
+            echo "<img src='http://www.gravatar.com/avatar/{$userAccount["public_data"]["gravatarID"]}' />";
+        if(isset($userAccount["delegate_info"]))
+            echo "<p>This is the VIP section, for delegates only.</p>";
     } else {
-        $url = rpc("wallet_login_start", [$BITSHARES_USER_NAME]);
+        $url = $bitshares->wallet_login_start($BITSHARES_USER_NAME);
         $url .= $LOGIN_PAGE_DOMAIN_AND_PATH;
         ?>
 BitShares will now prompt you to complete the login in a new window. You may now close this window.
@@ -57,5 +44,4 @@ BitShares will now prompt you to complete the login in a new window. You may now
         <?PHP
     }
 
-    socket_close($sock);
 ?>
