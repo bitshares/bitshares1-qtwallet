ca607cbc3155f8133a7c3ed806875a9a14a6af14
dnotestein <dan@syncad.com>
2014-10-10 16:25:55 -0400

    Fix intermittent crash during database open at client startup in wallet GUI when boost::filesystem::path call is made from bitshares thread. To solve this problem, we must initialize boost filesystem global variables with the std::locale in the main function prior to making any boost path calls.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include "BitSharesApp.hpp"
+#include <boost/filesystem.hpp>
 
 int main( int argc, char** argv )
 {
+  #ifdef WIN32
+    // Due to a bug in boost that we don't fully understand, we get errors in boost::filesystem::path
+    // when used from a thread if we don't do this first.
+    boost::filesystem::path::imbue(std::locale());
+  #endif
   return BitSharesApp::run(argc, argv);
 }
