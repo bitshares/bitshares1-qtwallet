1f67f2ef6c4d29bd0efa1c9b6b5b1760ef68b455
Vikram Rajkumar <vikram@bitshares.org>
2014-11-10 14:27:36 -0500

    More BTSX(T) -> BTS renaming

--- BitSharesApp.cpp
+++ BitSharesApp.cpp
@@ -265,7 +265,7 @@ void uninstallCrashRptHandler()
 
 BitSharesApp* BitSharesApp::_instance = nullptr;
 
-#define APP_NAME "BitSharesXT"
+#define APP_NAME "BitShares"
 
 static std::string CreateBitSharesVersionNumberString()
 {
