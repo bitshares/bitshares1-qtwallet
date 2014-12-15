c08a5fd14db980740e137876cd38f08a0eb7baa4
valzav <i@valzav.com>
2014-12-15 14:39:51 -0500

    pass plain string open_in_external_browser in order to call it from js

--- Utilities.cpp
+++ Utilities.cpp
@@ -7,23 +7,28 @@
 #include <QDesktopServices>
 #include <QFileDialog>
 
-void Utilities::copy_to_clipboard(QString string)
+void Utilities::copy_to_clipboard(const QString& string)
 {
     qApp->clipboard()->setText(string);
 }
 
-void Utilities::open_in_external_browser(QUrl url)
+void Utilities::open_in_external_browser(const QString& url)
+{
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void Utilities::open_in_external_browser(const QUrl& url)
 {
     QDesktopServices::openUrl(url);
 }
 
-QString Utilities::prompt_user_to_open_file(QString dialogCaption)
+QString Utilities::prompt_user_to_open_file(const QString& dialogCaption)
 {
     return QFileDialog::getOpenFileName(nullptr, dialogCaption, QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation).first());
 }
 
 
-void Utilities::log_message(QString message)
+void Utilities::log_message(const QString& message)
 {
     wlog("Message from GUI: ${msg}", ("msg",message.toStdString()));
 }
